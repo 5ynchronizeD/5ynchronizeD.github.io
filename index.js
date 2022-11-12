0ce01d152dbc6b80f2e88b054fdc94589d6c5dfa
Oscar Ragnerby <oscar.ragnerby@obos.se>
2022-11-12 15:25:13 +0100

    ba

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     let btn_counter = document.querySelector('#btn-counter');
-    btn_counter.addEventListener('click', count());
+    btn_counter.addEventListener('click', function() {
+        alert('Bla');
+    });
 
 });
-let counter = 0;
+
 
 function count() {
-    counter++;
     let txt_counter = document.querySelector('#txt-counter');
+    let counter = txt_counter.innerHTML;
+    counter++;
     //let string = 'Count is now ${counter}';
     txt_counter.innerHTML = 'Count is now ' + counter;
 }
