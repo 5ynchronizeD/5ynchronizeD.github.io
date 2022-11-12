e96bf00d480f38054629bb8ddb6a7a3c4a0cc3e9
Oscar Ragnerby <oscar.ragnerby@obos.se>
2022-11-12 15:20:49 +0100

    Modified functions

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    let btn_counter = document.querySelector('#btn-counter');
+    btn_counter.addEventListener('click', count());
+
+});
 let counter = 0;
 
 function count() {
     counter++;
-    document.querySelector('h1').innerHTML = 'Count is now ${counter}';
+    let txt_counter = document.querySelector('#txt-counter');
+    //let string = 'Count is now ${counter}';
+    txt_counter.innerHTML = 'Count is now ${counter}';
 }
